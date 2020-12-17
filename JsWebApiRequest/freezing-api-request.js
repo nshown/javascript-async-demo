56ff3df329ba2bd0838fab25686b12d6c2d7f49b
Nathan Shown <nathan.shown@gmail.com>
2020-12-17 01:56:38 -0500

    I've added my initial code to illustrate JavaScript's async functionality

---
+++ JsWebApiRequest/freezing-api-request.js
@@ -0,0 +1,70 @@
+const coindesk_url = "https://api.coindesk.com/v1/bpi/historical/close.json?start=2020-01-01&end=2020-02-29";
+const slowwly_url = "http://slowwly.robertomurray.co.uk/delay/8000/url/";
+const web_api_url = slowwly_url + coindesk_url;
+
+var data = [];
+var layout = {};
+
+function displayPlot(){
+  Plotly.newPlot("plot-here", data, layout, {displayModeBar: false});
+}
+
+function getDataSynchronous()
+{
+  var request = new XMLHttpRequest();
+  request.open('GET', web_api_url, false);  // `false` makes the request synchronous
+  request.send(null);
+
+  if (request.status === 200) {
+    console.log(request.responseText);
+  }
+  else{
+    console.log("I wasn't able to get data from that url")
+    return;
+  }
+
+  var requestJson = JSON.parse(request.responseText);
+  var bpiJson = requestJson.bpi;
+
+  myTrace = {
+    x: Object.keys(bpiJson),
+    y: Object.values(bpiJson),
+    type: 'scatter'
+  }
+
+  data = [myTrace];
+
+  layout = {
+    autosize:true,
+  }
+
+  displayPlot();
+
+  console.log("Finally Finished!");
+}
+
+function styleChart(){
+  if (data.length !== 0) {
+    var btnText = d3.event.target.innerText.toLowerCase();
+
+    if (btnText === 'dark') {
+      data[0].line = {color: '#7F7F7F'};
+    
+      layout = {
+        autosize:true,
+        plot_bgcolor:"black",
+      }
+    } else {
+      data[0].line = {color: 'dark blue'};
+    
+      layout = {
+        autosize:true,
+      }
+    }
+
+    displayPlot();
+  }
+}
+
+d3.select("#get-data-btn").on("click", getDataSynchronous);
+d3.selectAll(".style-plot-btn").on("click", styleChart);
--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# javascript-async-demo
-This is a simple demo to illustrate the importance of JavaScript's functional programming style and how it enables webpages to function
+# Javascript Async Demo
+This is a simple demo to illustrate the importance of JavaScript's functional programming style and how it enables webpages to function.  I use Jupyter, D3, Plotly, and Bootstrap to illustrate how JavaScript elegantly handles long running functions.
+
+
+---
+
+
+Most introduction to programming involves running a series of code and noting the results.  If a particular line of code takes a long time to run this isn't usually too disruptive to your coding process, you wait for the code to finish and then you continue coding.  If you wanted to plot data from a slow Web API your [Jupyter Notebook](JupyterExample\web_api_test.ipynb) just takes a bit longer to finish running.
+
+Unfortunately, if the code you're writing drives a user interface, long running functions can have the effect of [freezing your UI](JsWebApiRequest/index.html) until those functions have completed.  
+
+JavaScript offers a range of functionality to address this issue.  Promises set aside functions until they have completed their initial task.  During this time the webpage functions as normal.  When the initial task has completed the Promise will execute the functions it has been passed, often processing data it has collected.  This allows for [calls to slow Web APIs](D3Ajax/index.html) without freezing your webpages.
